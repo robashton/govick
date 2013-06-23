6d6c434be49ac234b0f4cae0f42b0a5653385e35
Rob Ashton <robashton@codeofrob.com>
2013-06-23 12:43:39 +0100

    First iteration of liberty calculation

--- board.js
+++ board.js
@@ -25,13 +25,19 @@ Board.prototype = {
     this.placeStone(x,y,"black")
   },
   hasStoneAt: function(x,y) { 
-    return !!this._grid[this.indexFor(x,y)]
+    return !!this.colourAt(x,y)
   },
   isWhiteStoneAt: function(x,y){
-    return this._grid[this.indexFor(x,y)] === "white"
+    return  this.colourAt(x,y) === "white"
   },
   isBlackStoneAt: function(x,y){
-    return this._grid[this.indexFor(x,y)] === "black"
+    return  this.colourAt(x,y) === "black"
+  },
+  hasStoneWithColourAt: function(x,y,colour) {
+    return  this.colourAt(x,y) === colour
+  },
+  colourAt: function(x,y) {
+    return this._grid[this.indexFor(x,y)]
   },
   indexFor: function(x,y) {
     return x + y * this._size
@@ -43,17 +49,34 @@ Board.prototype = {
     return this._lastMessage
   },
   libertiesFor: function(x,y) {
-    var liberties = 0
-    liberties += this.collectLibertiesFor(x-1, y)
-    liberties += this.collectLibertiesFor(x+1, y)
-    liberties += this.collectLibertiesFor(x, y+1)
-    liberties += this.collectLibertiesFor(x, y-1)
-    return liberties
+    return this.seekLibertiesAround(x,y,{},0)
   },
-  collectLibertiesFor: function(x,y) {
-    if(x < 0 || y < 0) return 0
-    if(x >= this._size || y >= this._size) return 0
-    return this.hasStoneAt(x,y) ? 0 : 1
-  }
+  seekLibertiesAround: function(x,y,visited,count) {
+    if(this.touchCacheFor(x,y,visited)) return count
+    var colour = this.colourAt(x,y)
+    count = this.collectLibertiesFor(x-1, y, colour, visited, count)
+    count = this.collectLibertiesFor(x+1, y, colour, visited, count)
+    count = this.collectLibertiesFor(x, y+1, colour, visited, count)
+    count = this.collectLibertiesFor(x, y-1, colour, visited, count)
+    return count
+  },
+  touchCacheFor: function(x,y, visited) {
+    var key = this.createCacheKeyFor(x,y)
+    if (visited[key]) return true
+    visited[key] = true
+    return false
+  },
+  collectLibertiesFor: function(x,y, colour, visited, count) {
+    if(x < 0 || y < 0) return count
+    if(x >= this._size || y >= this._size) return count
+    if(!this.hasStoneAt(x,y)) return count+1
+    if(this.hasStoneWithColourAt(x,y, colour)) {
+      return this.seekLibertiesAround(x,y, visited, count)
+    }
+    return count
+  },
+  createCacheKeyFor: function(x,y) {
+    return [x,y].join(",")
+  },
 }
 module.exports = Board
--- test/liberties.coffee
+++ test/liberties.coffee
@@ -57,20 +57,38 @@ Scenario "Placing a stone in the middle of the board and asking for its libertie
   setupCell([1..7], [1..7])
 
 
-Scenario "The liberties of two adjacent stones", ->
+Scenario "The liberties of two adjacent stones of different colours", ->
   board = null
 
   Given "A board with 9x9 lines", ->
     board = new Board(9)
 
-  When "Placing a stone on 5x5", ->
-    board.placeStone(5,5)
+  When "Placing a white stone on 5x5", ->
+    board.placeWhiteStone(5,5)
 
-  And "Placing a stone on 5x6", ->
-    board.placeStone(5,6)
+  And "Placing a black stone on 5x6", ->
+    board.placeBlackStone(5,6)
   
   Then "The stone 5x5 should have 3 liberties", ->
     board.libertiesFor(5,5).should.equal(3)
 
   Then "The stone 5x6 should have 3 liberties", ->
     board.libertiesFor(5,6).should.equal(3)
+
+Scenario "The liberties of two adjacent stones of the same colour", ->
+  board = null
+
+  Given "A board with 9x9 lines", ->
+    board = new Board(9)
+
+  When "Placing a white stone on 5x5", ->
+    board.placeWhiteStone(5,5)
+
+  And "Placing a white stone on 5x6", ->
+    board.placeWhiteStone(5,6)
+  
+  Then "The stone 5x5 should have 6 liberties", ->
+    board.libertiesFor(5,5).should.equal(6)
+
+  Then "The stone 5x6 should have 6 liberties", ->
+    board.libertiesFor(5,6).should.equal(6)
