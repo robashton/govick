45cda6d69fd57fcde8d50fe67be8ee9a4cc1f972
Rob Ashton <robashton@codeofrob.com>
2013-06-23 15:58:32 +0100

    Object literals for the two players

--- game.js
+++ game.js
@@ -1,19 +1,28 @@
 var Game = function(board) {
  this._board = board
- this._currentPlayer = "black"
+ this._currentPlayer = BlackPlayer
  this._board.on("stone-placed", this.onStonePlaced.bind(this))
 }
 
 Game.prototype = {
   placeStone: function(x,y) {
-    if (this._currentPlayer === "black") { 
-      this._board.placeBlackStone(x,y)
-    } else {
-      this._board.placeWhiteStone(x,y)
-    }
+    this._currentPlayer.move.call(this, x, y)
   },
   onStonePlaced: function() {
-    this._currentPlayer = this._currentPlayer === "black" ? "white" : "black"
+    this._currentPlayer = this._currentPlayer.next()
   }
 }
+
+
+var BlackPlayer = {
+  move: function(x,y) { this._board.placeBlackStone(x,y) },
+  next: function() { return WhitePlayer }
+}
+
+var WhitePlayer = {
+  move: function(x,y) { this._board.placeWhiteStone(x,y) },
+  next: function() { return BlackPlayer }
+}
+
+
 module.exports = Game
